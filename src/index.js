322e981a9f4560f8c4c5ec4db6b2a1b09a095836
gersonvidal18 <zs22004340@estudiantes.uv.mx>
2024-05-16 20:49:50 -0600

    update uri

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // 2
 const httpLink = createHttpLink({
-  uri: 'http://localhost:8000/graphql/'
+  uri: 'http://http://34.174.57.226:8080/graphql/'
 });
 
 const authLink = setContext((_, { headers }) => {
